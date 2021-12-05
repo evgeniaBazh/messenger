15ef4f7e0ab6ec2ad0d2ba21dbc18911050d0c4c
Aleksey Polchikov <keksley@gmail.com>
2021-12-05 23:00:05 +0500

    Add shared input module

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { InputTypes } from 'src/app/types/input-types.enum';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent implements OnInit {
+  get types(): typeof InputTypes {
+    return InputTypes;
+  }
+  constructor() { }
+  @Input()
+  contentClass: string = '';
+
+  @Input()
+  type: string = InputTypes.text;
+
+  @Input()
+  autocomplete: string = '';
+
+  @Input()
+  name?: string;
+
+  // Эмуляция ngModel
+  @Input('ngModel')
+  public model: string = ''
+
+  @Output('ngModelChanged')
+  public modelChanged: EventEmitter<string> = new EventEmitter();
+
+  visibility: boolean = false;
+
+  get currentType(): InputTypes {
+    if (this.type !== InputTypes.password) {
+      return InputTypes[this.type as InputTypes];
+    }
+    return this.visibility ? InputTypes.text : InputTypes.password;
+  }
+
+
+  ngOnInit(): void {
+  }
+
+}

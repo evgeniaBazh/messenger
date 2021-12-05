da4f048a214aff3b7df8a80dfbaffd9fe713ce75
Aleksey Polchikov <keksley@gmail.com>
2021-12-05 21:18:40 +0500

    Add tabs logic

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+enum TabNames {
+  LOGIN,
+  REGISTER,
+}
+
+interface Tab {
+  label: string;
+  name: TabNames;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  tabs: Array<Tab> = [
+    {
+      label: 'Войти',
+      name: TabNames.LOGIN
+    },
+    {
+      label: 'Зарегистрироваться',
+      name: TabNames.REGISTER,
+    }
+  ]
+  currentTab: TabNames = TabNames.LOGIN;
+  get tabNames(): typeof TabNames {
+    return TabNames;    
+  }
 
   constructor() { }
-
+  onTabClick(currentTab: TabNames) {
+    this.currentTab = currentTab
+  }
   ngOnInit(): void {
   }
 

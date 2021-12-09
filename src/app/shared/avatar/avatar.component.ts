5021997e1943361428b2c5d2a74acd88a84dc9c4
evgeniaBazh <evgeniabazh7@gmail.com>
2021-12-09 12:20:50 +0500

    обавлен компонент модальное окно настроек

--- src/app/shared/avatar/avatar.component.ts
+++ src/app/shared/avatar/avatar.component.ts
@@ -9,8 +9,23 @@ export class AvatarComponent implements OnInit {
   @Input() src = 'assets/img/avatar.png';
   @Input() size = '68px';
   @Input() status: 'online' | 'inactive' | null = null;
-
+  @Input() editable: boolean = false;
+  @Input() contentId?: string;
+  fileReader: FileReader = new FileReader();
   constructor() {}
+
+  onChooseAvatar(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event: any) => {
+        // called once readAsDataURL is completed
+        this.src = event.target.result;
+      };
+    }
+  }
 
   ngOnInit(): void {}
 }

f6d4f3276c61548d3b394ab5b404e2704751f29d
Алексей Польчиков <keksley@gmail.ru>
2021-12-14 21:19:05 +0500

    Установлен angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AvatarComponent } from './avatar/avatar.component';
 import { DialogueComponent } from './dialogue/dialogue.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,13 @@ import { DialogueComponent } from './dialogue/dialogue.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'full-shrek-messenger',
+    appId: '1:394401023186:web:d2c9a8a11cfa6391d6d90b',
+    storageBucket: 'full-shrek-messenger.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyDEP2Or8C84DM9xxk1tX2Oi2xlfR0_9CkU',
+    authDomain: 'full-shrek-messenger.firebaseapp.com',
+    messagingSenderId: '394401023186',
+    measurementId: 'G-2P6RHWXD6H',
+  },
   production: true
 };
